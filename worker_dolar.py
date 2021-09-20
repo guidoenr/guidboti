14683bc709f0f848542e79a69e9a2a986fd63371
guidoenr <guidoenr4@gmail.com>
2021-09-20 17:27:04 -0300

    scheduler + upgrades

--- worker_dolar.py
+++ worker_dolar.py
@@ -1,13 +1,14 @@
 #import bot
-import datetime
 import constants
 import requests
-from datetime import datetime as dt
+from apscheduler.schedulers.blocking import BlockingScheduler
 
+scheduler = BlockingScheduler(timezone='UTC-3')
 INTERVAL = constants.INTERVAL_1_HOURS
 
 
 # returns compra-venta, in that order
+@scheduler.scheduled_job('cron', day_of_week='mon-tue-wed-thu-fri')
 def get_dollar_status():
     response = requests.request('GET', constants.URI_DOLARSI)
     if response.status_code != 200:
@@ -16,4 +17,4 @@ def get_dollar_status():
     mep_dict = [x for x in full_dict if x['casa']['nombre'] == 'Dolar Bolsa'][0]['casa']
     return mep_dict['compra'], mep_dict['venta']
 
-print(datetime.datetime.now())
+
