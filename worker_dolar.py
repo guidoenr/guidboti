49ff0ed55b0e2b0715a5be186a09d9412554fad4
guidoenr <guidoenr4@gmail.com>
2021-09-20 20:17:03 -0300

    prueba de tweet

--- worker_dolar.py
+++ worker_dolar.py
@@ -1,14 +1,16 @@
-#import bot
+import bot
+import datetime as dt
 import constants
 import requests
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-scheduler = BlockingScheduler(timezone='UTC-3')
-INTERVAL = constants.INTERVAL_1_HOURS
+scheduler = BlockingScheduler(timezone='America/Argentina/Buenos_Aires')
+start_date = dt.datetime.now().replace(hour=20, minute=13, second=0, microsecond=0)
+end_date = dt.datetime.now().replace(hour=20, minute=20, second=0, microsecond=0)
 
 
 # returns compra-venta, in that order
-@scheduler.scheduled_job('cron', day_of_week='mon-tue-wed-thu-fri')
+# @scheduler.scheduled_job('interval', minutes=30, start_date=start_date, end_date=end_date)
 def get_dollar_status():
     response = requests.request('GET', constants.URI_DOLARSI)
     if response.status_code != 200:
@@ -18,3 +20,10 @@ def get_dollar_status():
     return mep_dict['compra'], mep_dict['venta']
 
 
+@scheduler.scheduled_job('interval', seconds=67, start_date=start_date, end_date=end_date)
+def test_to_print():
+    n = dt.datetime.now().minute
+    bot.send_tweet("[scheduler_test]: a ver q onda: " + str(n))
+
+
+scheduler.start()
