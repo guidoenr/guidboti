2fb2fb5b38b3ee58649d2b155c22510a8c21fd57
guidoenr <guidoenr4@gmail.com>
2021-09-21 17:26:46 -0300

    asd

--- workers/sched_workers.py
+++ workers/sched_workers.py
@@ -4,18 +4,23 @@ from apscheduler.triggers.cron import CronTrigger
 from apscheduler.triggers.combining import AndTrigger
 import datetime as dt
 import worker_dollar as wd
-
 import bot
 
-blockingScheduler = BlockingScheduler(timezone='America/Argentina/Buenos_Aires')
+scheduler = BlockingScheduler(timezone='America/Argentina/Buenos_Aires')
 
-trigger_interval = IntervalTrigger(seconds=20, start_date=wd.DATE_START, end_date=wd.DATE_END)
-trigger_cron = CronTrigger(day_of_week='mon,tue,wed')
-trigger_combined = AndTrigger([trigger_interval, trigger_cron])
+trigger_interval = IntervalTrigger(minutes=20)
+trigger_cron = CronTrigger(day_of_week='mon,tue,wed,thu,fri')
 
 
 def test_fun():
     bot.log("TEST_FUNC")
+
+
+test_job = scheduler.add_job(test_fun, trigger=trigger_cron)
+scheduler.start()
+
+
+
 
 
 
