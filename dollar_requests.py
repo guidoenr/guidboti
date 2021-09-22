c59213dd5ecda32dd9bc367ac25981198e9ae11f
guidoenr <guidoenr4@gmail.com>
2021-09-22 14:14:28 -0300

    new version: tweet only if the dollar value change

--- dollar_requests.py
+++ dollar_requests.py
@@ -4,10 +4,9 @@ import requests
 from datetime import datetime as dt
 
 dollar_template = """
-Dollar MEP status at {}
+Dollar MEP status changed at {}
 Compra: {}
 Venta: {}
-@guidoenr
 """
 
 
@@ -15,16 +14,29 @@ def post_dollar_status():
     response = requests.request('GET', constants.URI_DOLARSI)
     assert response.status_code == 200
     full_dict = response.json()
-    to_tweet = tweet_format(full_dict)
-    bot.send_tweet(to_tweet)
-
-
-# returns compra-venta values, in that order
-def tweet_format(dict_response):
-    mep_dict = [x for x in dict_response if x['casa']['nombre'] == 'Dolar Bolsa'][0]['casa']
+    mep_dict = [x for x in full_dict if x['casa']['nombre'] == 'Dolar Bolsa'][0]['casa']
     compra, venta = mep_dict['compra'], mep_dict['venta']
-    hour, minute = dt.now().hour, dt.now().minute
-    current_time = "{}:{}".format(hour, minute)
-    to_tweet = dollar_template.format(current_time, compra, venta)
-    return to_tweet
+    if check_dollar_value(compra, venta):
+        update_dollar_value(compra, venta)
+        hour, minute = dt.now().hour, dt.now().minute
+        current_time = "{}:{}".format(hour, minute)
+        to_tweet = dollar_template.format(current_time, compra, venta)
+        #bot.send_tweet(to_tweet)
+        bot.log(to_tweet)
+    else:
+        bot.log("DOLLAR MEP status doesn't changed")
+
+
+def check_dollar_value(new_compra, new_venta):
+    stats_file = open(file='stats.dat', mode='r')
+    old_compra = stats_file.readline()
+    old_venta = stats_file.readline()
+    stats_file.close()
+    return True if (str(new_compra), str(new_venta)) != (old_compra.replace('\n', ''), old_venta) else False
+
+
+def update_dollar_value(new_compra, new_venta):
+    with open(file='stats.dat', mode='w') as new_file:
+        new_file.write(str(new_compra) + '\n')
+        new_file.write(str(new_venta))
 
--- worker_scheduler.py
+++ worker_scheduler.py
@@ -6,18 +6,18 @@ import dollar_requests as wd
 scheduler = BlockingScheduler(timezone='America/Argentina/Buenos_Aires')
 
 trigger_interval = IntervalTrigger(minutes=15)
-trigger_cron_start = CronTrigger(day_of_week='mon,tue,wed,thu,fri', hour=11, minute=30)
-trigger_cron_stop = CronTrigger(day_of_week='mon,tue,wed,thu,fri', hour=18, minute=0)
+
+# trigger_cron_start = CronTrigger(day_of_week='mon,tue,wed,thu,fri', hour=11, minute=30)
+# trigger_cron_stop = CronTrigger(day_of_week='mon,tue,wed,thu,fri', hour=18, minute=0)
 
 
-def start_interval():
-    scheduler.add_job(id='tweet_dollar', func=wd.post_dollar_status, trigger=trigger_interval)
+# def start_interval():
+#    scheduler.add_job(id='tweet_dollar', func=wd.post_dollar_status, trigger=trigger_interval)
 
 
-def stop_interval():
-    scheduler.remove_job(job_id='tweet_dollar')
+# def stop_interval():
+#    scheduler.remove_job(job_id='tweet_dollar')
 
 
-job_start_interval = scheduler.add_job(start_interval, trigger=trigger_cron_start)
-job_stop_interval = scheduler.add_job(stop_interval, trigger=trigger_cron_stop)
+scheduler.add_job(func=wd.post_dollar_status, trigger=trigger_interval)
 scheduler.start()
